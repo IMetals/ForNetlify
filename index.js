96db9fdc592804c796e8317650759e3b2aa0a375
Metals <nguyenminhnhat.embedded@gmail.com>
2025-05-11 21:31:46 +0700

    edit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const contentLetterSrart_actived = "Hãy nói gì đó ở đây trước khi người ấy mở bức thư nhé." //Lời mở đầu cho bức thư
-const mainContentLetter = "Gửi lời nhắn nhủ đến người bạn bạn yêu thương." //Nội dung của bức thư
+const contentLetterSrart_actived = "Thông báo! Hôm nay là ngày 14-5-2025." //Lời mở đầu cho bức thư
+const mainContentLetter = "Chúc vợ yêu sinh nhật vui vẻ. Luôn luôn xinh đẹp và đáng yêu." //Nội dung của bức thư
 
 // Gắn 1 đường link ảnh bất kì
 let imgStart = document.querySelector(".myAI"); //Hình ảnh xuất hiện trong lời mở đầu của bức thư
